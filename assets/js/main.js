da4c462014ad39e9fe0bc4c3b6a87e41d8b31acd
Donnovan Feuillastre <donnovan42320@gmail.com>
2021-12-04 17:17:42 +0100

    Commit all necessary to begin portfolio tutorial

---
+++ assets/js/main.js
@@ -0,0 +1,93 @@
+// Homepage component
+const Home = {
+    template: `<main id="home">
+        <div class="about__me">
+            <img src="./assets/img/avatar.svg" alt="">
+            <h1>John Doe</h1>
+            <h3>Python Expert</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+    
+            <div class="skills_projects_link">
+                <router-link to="/projects">Projects/Skills</router-link> 
+            </div>
+        </div>
+    </main>`,
+};
+
+const Projects = {
+    template: `<div>
+        <h1>Projects</h1>
+    </div>`,
+
+    data() {
+        return {
+            // data object here
+            projects: [],
+            projectsList: null,
+            skills: [],
+            projectsCount: 5,
+            perPage: 20,
+            page: 1,
+            loading: true,
+            errors: false,
+        }
+    },
+
+    methods: {
+        // all methods here
+        fetchData: function() {
+            axios
+                .get(`https://api.github.com/users/PhantomDO/repos?per_page=${this.perPage}&page=${this.page}`)
+                .then(
+                    response => {
+                        this.projects = response.data;
+                        this.projects.array.forEach(project => {
+                            if (project.language !== null && !this.skills.includes(project.language)) {
+                                this.skills.push(project.language);
+                            }
+                        });
+                    }
+                )
+                .catch(error => {
+                    console.log(error);
+                    this.errors = true;
+                })
+                .finally(() => {
+                    this.loading = false;
+                    this.getProjects();
+                })
+        },
+
+        getProjects: function() {
+            this.projectsList = this.projects.array.slice(0, this.projectsCount);
+            return this.projectsList;
+        },
+
+        loadMore: function() {
+            if (this.projectsList.array.length <= this.projects.array.length) {
+                this.projectsCount += 5;
+                this.projectsList = this.getProjects();
+            }
+        }
+    },
+
+    mounted() {
+        // lifecycle hook
+    },
+};
+
+// defines routes
+const routes = [
+    { path: '/', component: Home },
+    { path: '/projects', component: Projects }
+];
+
+// create router instance
+const router = new VueRouter({
+    routes
+})
+
+// create and mount the vue instance
+const app = new Vue({
+    router
+}).$mount('#app');
